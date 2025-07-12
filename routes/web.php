b1a63e4c4b08ff97350c4e4bd4065a899700f85e
=Mudit Gulgulia <gulgulia17@gmail.com>
2025-07-12 22:25:15 +0530

    Products Added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    $products = \App\Models\Product::all();
+    return view('products.index', ['products' => $products]);
+});
